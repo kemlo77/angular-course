a6616df7a997c11020189ef53d75e127077577ce
Fredrik Kemling <kemling@gmail.com>
2021-02-02 15:20:16 +0100

    lectures up until 59

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Hamburgare', 'Mmm... hamburgare', 'https://cdn.pixabay.com/photo/2018/10/30/09/28/burger-3783181_960_720.jpg'),
+    new Recipe('Pizza', 'Ååååh pizza', 'https://cdn.pixabay.com/photo/2016/07/10/16/05/pizza-1507939_960_720.png')
+  ];
 
   constructor() { }
 
